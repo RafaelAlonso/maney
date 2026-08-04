af83422d7677669879a9a5f7db7750635a4771a9
Rafael Alonso <rafael.palonso@gmail.com>
2026-08-04 18:58:45 -0300

    feat: make Maney installable with its own name, colours and icons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
   root "home#show"
   get  "setup", to: "setup#new"
   post "setup", to: "setup#create"
